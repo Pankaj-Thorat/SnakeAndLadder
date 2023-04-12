6a10e11174934aa3326e4ef0e3e3f2357cc6d8a5
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-12 19:35:58 +0530

    [Pankaj] Start position for player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        public static int startPosition = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("player started at position {0}", startPosition);
+        }
+    }
+}

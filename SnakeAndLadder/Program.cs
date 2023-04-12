e6609a70ac2e1d5bb42d697da6b7d2d0fd8c9f73
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-12 23:31:44 +0530

    [Pankaj] added Player 2 and finel winner

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,18 +6,17 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            int position = 0;
+            int[] positions = { 0, 0 };
             int winningPosition = 100;
             const int snake = 1;
             const int ladder = 2;
             const int noPlay = 3;
-            int noOfTurns = 0;
             Random r = new Random();
+            int currentPlayer = 0;
+            bool gotExtraChance = false;
 
-            while (position < winningPosition)
-            {   
-                //to get the number of times the dice was played
-                noOfTurns++;
+            while (true)
+            {
                 //roll the die and check for the options using RANDOM
                 int dieNo = r.Next(1, 7);
                 int options = r.Next(1, 4);
@@ -25,35 +24,55 @@ namespace SnakeAndLadder
                 switch (options)
                 {
                     case noPlay:
-                        Console.WriteLine("No Play! You stay in the same position");
+                        Console.WriteLine("No Play! Player {0} stays in the same position", currentPlayer + 1);
                         break;
                     case ladder:
                         //to get the exact 100
-                        int newPosition = position + dieNo;
+                        int newPosition = positions[currentPlayer] + dieNo;
                         if (newPosition <= winningPosition)
                         {
-                            position = newPosition;
-                            Console.WriteLine("Ladder! You move ahead by {0} positions", dieNo);
+                            positions[currentPlayer] = newPosition;
+                            Console.WriteLine("Ladder! Player {0} moves ahead by {1} positions", currentPlayer + 1, dieNo);
+                            gotExtraChance = true;
                         }
                         else
                         {
-                            Console.WriteLine("Ladder! You need {0} to win. Stay where you are.", winningPosition - position);
+                            Console.WriteLine("Ladder! Player {0} needs {1} to win. Stay where you are.", currentPlayer + 1, winningPosition - positions[currentPlayer]);
                         }
                         break;
                     case snake:
-                        Console.WriteLine("Snake! You move behind by {0} positions", dieNo);
-                        position -= dieNo;
+                        Console.WriteLine("Snake! Player {0} moves behind by {1} positions", currentPlayer + 1, dieNo);
+                        positions[currentPlayer] -= dieNo;
+                        gotExtraChance = false;
                         break;
                 }
                 //check if player's position is within valid range
-                if (position < 0)
+                if (positions[currentPlayer] < 0)
                 {
-                    position = 0;
+                    positions[currentPlayer] = 0;
                 }
-                Console.WriteLine("Your current position is {0}", position);
+                Console.WriteLine("Player {0}'s current position is {1}", currentPlayer + 1, positions[currentPlayer]);
+                //check if player has reached the winning position
+                if (positions[currentPlayer] == winningPosition)
+                {
+                    Console.WriteLine("Congrats!! Player {0} has reached the winning position of {1}", currentPlayer + 1, winningPosition);
+                    break;
+                }
+                //switch to the next player if the current player did not get an extra chance
+                if (!gotExtraChance)
+                {
+                    currentPlayer = (currentPlayer + 1) % 2;
+                }
             }
-            Console.WriteLine("Congrats!! You have reached the winning position of {0} by playing {1} turns.", winningPosition,noOfTurns);
-
+            //report the winner
+            if (positions[0] == winningPosition)
+            {
+                Console.WriteLine("Player 1 has won the game!");
+            }
+            else if (positions[1] == winningPosition)
+            {
+                Console.WriteLine("Player 2 has won the game!");
+            }
         }
     }
 }
